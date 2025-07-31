db1c5abcc30589df7f005a05160dee975bde441e
gunnerdumas <gcookdumas@umassd.edu>
2025-07-31 10:21:16 -0400

    Added communcation with the Espon Arm

--- epsonvt6_sim/epson_joint_state.py
+++ epsonvt6_sim/epson_joint_state.py
@@ -9,6 +9,9 @@ from std_msgs.msg import Int8
 from robot_arm_interfaces.msg import RobotPosition, PrintingPoint
 import xacro
 import os
+import math
+import pandas as pd
+import numpy as np
 
 
 class JointStatePublisherNode(Node):
@@ -21,31 +24,66 @@ class JointStatePublisherNode(Node):
         urdf_desc = xacro.process_file(urdf_path)
         self.ik_chain = ikpy.chain.Chain.from_urdf_file(urdf_path, active_links_mask=[False, True, True, True, True, True, True])
 
-        #set same paramter types as the epson_vt6 script
-        self.declare_parameter("robot_position_topic", "robot_position")
-        self.declare_parameter("robot_initial_pos", [0.0, 700.0, 300.0, 0.0, 0.0, 0.0]) # x y z pitch roll yaw
-
         # create joint state publisher for rviz2
         self.joint_state_publisher = self.create_publisher(JointState, 'joint_states', 10)
         self.timer = self.create_timer(0.1, self.publish_joint_states) # Publish every 0.1 seconds
 
+
+        self.declare_parameter("robot_position_topic", "robot_position")
+        self.declare_parameter("robot_initial_pos", [0.0, 700.0, 250.0, 0.0, 0.0, 0.0])
         robot_position_topic = (
             self.get_parameter("robot_position_topic")
             .get_parameter_value()
             .string_value
         )
+        self.initial_position = (
+            self.get_parameter("robot_initial_pos")
+            .get_parameter_value()
+            .double_array_value
+        )
+        self.robot_tool = 3
+        self.robot_position = RobotPosition()
+        self.robot_position.tool_system.tool = self.robot_tool
+        self.robot_position.tool_system.pose.position.x = self.initial_position[0]
+        self.robot_position.tool_system.pose.position.y = self.initial_position[1]
+        self.robot_position.tool_system.pose.position.z = self.initial_position[2]
+        self.robot_position.tool_system.pose.orientation.x = self.initial_position[3]
+        self.robot_position.tool_system.pose.orientation.y = self.initial_position[4]
+        self.robot_position.tool_system.pose.orientation.z = self.initial_position[5]
 
-        self.robot_position_sub = self.create_subscription(RobotPosition, robot_position_topic, self.subscribe_robot_position, 2)
+        self.robot_position.camera_system.tool = self.robot_tool
+        self.robot_position.camera_system.pose.position.x = self.initial_position[0]
+        self.robot_position.camera_system.pose.position.y = self.initial_position[1]
+        self.robot_position.camera_system.pose.position.z = self.initial_position[2]
+        self.robot_position.camera_system.pose.orientation.x = self.initial_position[3]
+        self.robot_position.camera_system.pose.orientation.y = self.initial_position[4]
+        self.robot_position.camera_system.pose.orientation.z = self.initial_position[5]
 
+
+        self.robot_position_sub = self.create_subscription(
+            RobotPosition, robot_position_topic, self.robot_position_callback, 2
+        )
+
+
+        # Setup joints
         self.joint_names = ['base_swivel_joint', 'swivel_arm_joint', 'wrist_joint', 'hand_joint', 'finger_joint', 'tip_joint']
         self.joint_positions = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-                
-        # Set initial position
+
         self.initial_position = Pose()
-        self.initial_position = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+        self.initial_position.position.x = self.joint_positions[0]
+        self.initial_position.position.y = self.joint_positions[1]
+        self.initial_position.position.z = self.joint_positions[2]
+
+        self.initial_position.orientation.x = self.joint_positions[3]
+        self.initial_position.orientation.y = self.joint_positions[4]
+        self.initial_position.orientation.z = self.joint_positions[5]
 
         self.next_pose = Pose()
-        self.next_pose = self.initial_position
+        self.next_pose.position = self.initial_position.position
+        self.next_pose.orientation = self.initial_position.orientation
+                
+
+
 
 
     def publish_joint_states(self):
@@ -53,31 +91,63 @@ class JointStatePublisherNode(Node):
         joint_state_msg.header.stamp = self.get_clock().now().to_msg()
         joint_state_msg.name = self.joint_names
 
+
+
         joint_state_msg.position = self.joint_positions
         self.joint_state_publisher.publish(joint_state_msg)
         self.get_logger().info(f'Publishing Joint States: {self.joint_positions}')
-        self.get_logger().info(f'\ninital postion: {self.initial_position}')
+
 
     ## TODO: Find what publisher is posting the roboto position data (robot_position) but get system to work
     ## to see why nodes are not being published
-    def subscribe_robot_position(self, msg):
-        self.next_pose.position.x = msg.pose.position.x
-        self.next_pose.position.y = msg.pose.position.y
-        self.next_pose.position.z = msg.pose.position.z
+    def robot_position_callback(self, msg):
+        self.robot_position.tool_system.tool = msg.tool_system.tool
+        self.robot_position.tool_system.pose.position.x = (
+            msg.tool_system.pose.position.x
+        )
+        self.robot_position.tool_system.pose.position.y = (
+            msg.tool_system.pose.position.y
+        )
+        self.robot_position.tool_system.pose.position.z = (
+            msg.tool_system.pose.position.z
+        )
+        self.robot_position.tool_system.pose.orientation.x = (
+            msg.tool_system.pose.orientation.x
+        )
+        self.robot_position.tool_system.pose.orientation.y = (
+            msg.tool_system.pose.orientation.y
+        )
+        self.robot_position.tool_system.pose.orientation.z = (
+            msg.tool_system.pose.orientation.z
+        )
+      
 
-        self.next_pose.orientation.x = msg.pose.orientation.x
-        self.next_pose.orientation.y = msg.pose.orientation.y
-        self.next_pose.orientation.z = msg.pose.orientation.z
-        
         # the new joint angles
-        new_pose = self.ik_chain.inverse_kinematics([(self.next_pose.position.x) (self.next_pose.position.y) (self.next_pose.position.z)],
-                                           [(self.next_pose.orientation.x) (self.next_pose.orientation.y) (self.next_pose.orientation.z)])
-        self.joint_positions[0]=new_pose[1]
-        self.joint_positions[1]=new_pose[2]
-        self.joint_positions[2]=new_pose[3]
-        self.joint_positions[3]=new_pose[4]
-        self.joint_positions[4]=new_pose[5]
-        self.joint_positions[5]=new_pose[6]
+        posX=self.robot_position.tool_system.pose.position.x
+        posY=self.robot_position.tool_system.pose.position.y
+        posZ=self.robot_position.tool_system.pose.position.z
+        orX=self.robot_position.tool_system.pose.orientation.x
+        orY=self.robot_position.tool_system.pose.orientation.y
+        orZ=self.robot_position.tool_system.pose.orientation.z
+        position=np.array([posX, posY, posZ])
+        orientation=np.array([orX, orY, orZ])
+
+        self.get_logger().info(f'position: {position}')
+        self.get_logger().info(f'orientation: {orientation}')
+
+        # Error here  
+        self.next_pose = self.ik_chain.inverse_kinematics(position, orientation)
+        # And error here
+        self.joint_positions[0]=self.next_pose[1]
+        self.joint_positions[1]=self.next_pose[2]
+        self.joint_positions[2]=self.next_pose[3]
+        self.joint_positions[3]=self.next_pose[4]
+        self.joint_positions[4]=self.next_pose[5]
+        self.joint_positions[5]=self.next_pose[6]
+        # self.get_logger().info(f'subscribing States: {self.joint_positions}')
+
+        
+    
         
 
 def main(args=None):
